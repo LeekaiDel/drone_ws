a2d68bf9518de94b82c3917e5e0420c32b9311f7
LeekaiDel <antondel00@gmail.com>
2021-09-13 10:06:59 +0300

    что-то получается

--- src/packageInDev/scan_parser_test/scripts/cv_map_parser/scan_parser.py
+++ src/packageInDev/scan_parser_test/scripts/cv_map_parser/scan_parser.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import rospy
 from sensor_msgs.msg import LaserScan
-from sensor_msgs.msg import PointCloud2
+from sensor_msgs.msg import PointCloud2, PointField
 import sensor_msgs.point_cloud2 as pc2
 from laser_geometry import LaserProjection
 import os
@@ -10,6 +10,11 @@ import math
 from copy import deepcopy
 import matplotlib.pyplot as plt
 import numpy as np
+import pcl
+import sys
+import struct
+# import pcl_msgs
+import ctypes
 
 
 class Scan_Parser():
@@ -21,15 +26,13 @@ class Scan_Parser():
         self.laserSub = rospy.Subscriber("/scan", LaserScan, self.scan_cb)
         self.points_array = None
         self.d_functions = None
-        # self.timer = rospy.Timer(rospy.Duration(0.1), self.py_plot_cb)
+        self.timer = rospy.Timer(rospy.Duration(0.1), self.py_plot_cb)
 
     def py_plot_cb(self, timer=None):
         if self.d_functions is not None:
             # Рисуем графики
             ax1 = plt.subplot2grid((1, 1), (0, 0))
             ax1.plot(self.d_functions)
-            # ax1.plot(time_plot)
-            # ax1.set_title('Coords: drone X and goal X')
             ax1.set_ylabel('x, m')
             ax1.set_xlabel('angle, grad')
             ax1.legend()
@@ -38,15 +41,26 @@ class Scan_Parser():
 
     def scan_cb(self, msg):
         cloud_out = self.laserConverter.projectLaser(msg)
-        self.pc2ScanPub.publish(cloud_out)
+        # self.pc2ScanPub.publish(cloud_out)
         self.points_array = pc2.read_points_list(cloud_out)
+        PointCloudBlyat = pcl.PointCloud()
+        pc_list = []
+        for p in self.points_array:
+            pc_list.append([p[0], p[1], p[2]])
+        PointCloudBlyat.from_list(pc_list)
+        sor = PointCloudBlyat.make_voxel_grid_filter()
+        sor.set_leaf_size(0.01, 0.01, 0.01)
+        cloud_filtered = sor.filter()
+        # pcl_ros = pc2.create_cloud_xyz32(cloud_out.header, cloud_filtered)
+        print("pcl_ros: " + str(type(cloud_filtered)))
+        # self.pc2ScanPub.publish(pcl_ros)
+
         # self.marker_pub.publish(self.make_marker(self.points_array, 1))
         # os.system("clear")
-        self.d_functions = self.radius_filter(self.points_array, 5.0)
-        self.marker_pub.publish(self.make_contour(
-            self.d_functions, 2, (0.0, 1.0, 0.0)))
+        # self.d_functions = self.radius_filter(self.points_array, 5.0)
+        # self.marker_pub.publish(self.make_contour(
+        #     self.d_functions, 2, (0.0, 1.0, 0.0)))
         # print("\tDebug: " + str(self.d_functions))
-        print(self.pick_filter_(self.d_functions))
 
     def radius_filter(self, point_list: list, radius: float) -> list:
         array = list()
@@ -58,8 +72,7 @@ class Scan_Parser():
         for i in range(len(r_array)):
             if r_array[i] < radius:
                 array.append(point_list[i])
-
-        return array
+        return r_array
 
     def make_contour(self, point_list, id, rgb):
         marker = Marker()
