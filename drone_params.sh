7a91c99b30777e41642e5beef49a465ca487413a
LeekaiDel <antondel00@gmail.com>
2021-12-02 19:22:35 +0300

    mini fix

--- drone_params.sh
+++ drone_params.sh
@@ -1,23 +1,14 @@
 #!/bin/bash
 export DRONE_ID=1
 export DRONE_MARKER_ID=1
-export DRONE_IP=192.168.128.201
-
-export TEAM=0
-
+export DRONE_IP=192.168.1.254
 
 export DRONE_SAFE_MAX_ANGLE=85
 export DRONE_SAFE_DISARM_DELAY=0.2
 
-
-export ROSBRIDGE_SERVER_IP=''
-export ROSBRIDGE_SERVER_PORT=9090
-
 export MAVROS_GCS_IP=
 export MAVROS_FCU_URL='/dev/ttySAC0:921600'
 export MAVROS_FCU_PORT=14101
-
-export USE_MESH=False
 
 export DRONE_REG_USE_PLANNER=False
 export DRONE_REG_USE_GEO_MODE=False
