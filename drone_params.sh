f0d47e32fd0d44e657972a29eac0574adcb6ba82
LeekaiDel <antondel00@gmail.com>
2022-03-18 11:58:37 +0300

    fix drone params

--- drone_params.sh
+++ drone_params.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 export DRONE_ID=1
-export DRONE_MARKER_ID=1
-export DRONE_IP=192.168.1.253
+export DRONE_MARKER_ID=1            #
+export DRONE_IP=192.168.1.253       #
 
-export DRONE_SAFE_MAX_ANGLE=85
-export DRONE_SAFE_DISARM_DELAY=0.2
+export DRONE_SAFE_MAX_ANGLE=85      #
+export DRONE_SAFE_DISARM_DELAY=0.2  #
 
 export MAVROS_GCS_IP=
 export MAVROS_FCU_URL='/dev/ttyACM0:921600'
 export MAVROS_FCU_PORT=14101
 
-export DRONE_REG_USE_PLANNER=False
-export DRONE_REG_USE_GEO_MODE=False
+export DRONE_REG_USE_POTENTIAL_PLANNER=True    # Использовать потенциальные поля?
+export DRONE_REG_USE_UNSTABLE_PLANNER=True     # Использовать неустойчивый планировщик?
 
-export USE_POSE_REG=True
+#export USE_POSE_REG=True                        #
+
+export DRONE_REG_USE_GEO_MODE=False  #
+
+#export ROS_MASTER_URI=http://localhost:11311/
+#export ROS_IP=localhost
+#export ROS_HOSTNAME=localhost
 
