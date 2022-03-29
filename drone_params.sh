832207857e941e4564fb40dd0a32fbbb85ca8777
LeekaiDel <antondel00@gmail.com>
2022-03-29 15:27:10 +0300

    ununderstand fix

--- drone_params.sh
+++ drone_params.sh
@@ -6,12 +6,12 @@ export DRONE_IP=192.168.1.253       #
 export DRONE_SAFE_MAX_ANGLE=85      #
 export DRONE_SAFE_DISARM_DELAY=0.2  #
 
-export MAVROS_GCS_IP=
+export MAVROS_GCS_IP=192.168.128.253
 export MAVROS_FCU_URL='/dev/ttyACM0:921600'
 export MAVROS_FCU_PORT=14101
 
-export DRONE_REG_USE_POTENTIAL_PLANNER=True    # Использовать потенциальные поля?
-export DRONE_REG_USE_UNSTABLE_PLANNER=True     # Использовать неустойчивый планировщик?
+export DRONE_REG_USE_POTENTIAL_PLANNER=False    # Использовать потенциальные поля?
+export DRONE_REG_USE_UNSTABLE_PLANNER=False     # Использовать неустойчивый планировщик?
 
 #export USE_POSE_REG=True                        #
 
