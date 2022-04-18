d557de9fe1bd86fb2ff94a488c4a974f5692386e
LeekaiDel <antondel00@gmail.com>
2022-04-18 15:53:25 +0300

    changes in the drone_params.sh

--- drone_params.sh
+++ drone_params.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
+# export DRONE_MARKER_ID=1            #
+# export DRONE_IP=192.168.1.253       #
+# export DRONE_SAFE_MAX_ANGLE=85      #
+# export DRONE_SAFE_DISARM_DELAY=0.2  #
 export DRONE_ID=1
-export DRONE_MARKER_ID=1            #
-export DRONE_IP=192.168.1.253       #
 
-export DRONE_SAFE_MAX_ANGLE=85      #
-export DRONE_SAFE_DISARM_DELAY=0.2  #
-
+# Конфигурационные параметры для mavros
 export MAVROS_GCS_IP=192.168.128.253
 export MAVROS_FCU_URL='/dev/ttyACM0:921600'
 export MAVROS_FCU_PORT=14101
 
+# Флаги настройки системы:
 export DRONE_REG_USE_POTENTIAL_PLANNER=False    # Использовать потенциальные поля?
 export DRONE_REG_USE_UNSTABLE_PLANNER=False     # Использовать неустойчивый планировщик?
+export DRONE_REG_USE_GEO_MODE=False             # Использовать расчет позиции в нодах из координат GPS?          
 export USE_D435=False                           # Использовать камеру d435?
 
-export DRONE_REG_USE_GEO_MODE=False  #
-
+# Настройки для rosmaster 
 #export ROS_MASTER_URI=http://localhost:11311/
 #export ROS_IP=localhost
 #export ROS_HOSTNAME=localhost
