d0c4983b25685590491dabf192ebfa3eae287712
leekai <Leekai>
2022-04-20 14:01:07 +0300

    begin porting to ros2

---
+++ src/mavros_link/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+package_name = 'mavros_link'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='leekai',
+    maintainer_email='Leekai',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
