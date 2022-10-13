aa6cc10ac2dd493c0283ac29aef477dcc877b37a
Leekai <leekai.dev@gmail.com>
2022-10-13 13:08:31 +0300

    fix drone_params

--- drone_params.sh
+++ drone_params.sh
@@ -6,16 +6,4 @@ export MAVROS_GCS_IP=192.168.128.253
 export MAVROS_FCU_URL='/dev/ttyS0:115200'
 export MAVROS_FCU_PORT=14101
 
-# Флаги настройки системы:
-export DRONE_REG_USE_POTENTIAL_PLANNER=False    # Использовать потенциальные поля?
-export DRONE_REG_USE_UNSTABLE_PLANNER=False     # Использовать неустойчивый планировщик?
-export DRONE_REG_USE_GEO_MODE=False             # Использовать расчет позиции в нодах из координат GPS?          
-export USE_D435=False                           # Использовать камеру d435?
-
 export ROS_DOMAIN_ID=137
-
-# Настройки для rosmaster 
-#export ROS_MASTER_URI=http://localhost:11311/
-#export ROS_IP=localhost
-#export ROS_HOSTNAME=localhost
-
